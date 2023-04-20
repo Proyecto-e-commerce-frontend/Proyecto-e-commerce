f4c961505b44079a1008d41b490d5414f3948b0f
Hernán Felipe Pérez Díaz <hernanfelipeperezdiaz@gmail.com>
2023-04-19 20:10:28 -0500

    base login

--- src/app/Login/page.js
+++ src/app/Login/page.js
@@ -2,6 +2,24 @@ export default function Login() {
     
     return(
 
-        
+        <>
+
+            <form action="#" className="formulario-contacto">
+
+                <fieldset class="formulario-contacto__contenido">
+
+                    <legend class="formulario-contacto__contenido__titulo">Ingresar a mi cuenta</legend>
+                    <input type="email" class="formulario-contacto__contenido__nombre" placeholder="Escriba su correo electrónico" required data-input="email" />
+                    <span class="formulario-contacto__contenido__span"></span>
+                    <input class="formulario-contacto__contenido__nombre" type="password" placeholder="Escriba su contraseña" data-input="password" required />
+                    <span class="formulario-contacto__contenido__span"></span>
+                    <button class="formulario-contacto__contenido__boton" type="submit" formaction="./menu-administrador.html">Entrar</button>
+
+                </fieldset>
+
+            </form>
+
+        </>
+ 
     );
 }

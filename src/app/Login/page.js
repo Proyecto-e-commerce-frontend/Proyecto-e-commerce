5031f9a0885f1cc4b7afcef5c70fb87079f803e4
Hernán Felipe Pérez Díaz <hernanfelipeperezdiaz@gmail.com>
2023-04-20 19:39:14 -0500

    prueba

--- src/app/Login/page.js
+++ src/app/Login/page.js
@@ -2,24 +2,27 @@ export default function Login() {
     
     return(
 
+        /*parte del login */
         <>
 
-            <form action="#" className="formulario-contacto">
+            <form action="#" className="">
 
-                <fieldset class="formulario-contacto__contenido">
+                <fieldset className="formulario-contacto__contenido">
 
-                    <legend class="formulario-contacto__contenido__titulo">Ingresar a mi cuenta</legend>
-                    <input type="email" class="formulario-contacto__contenido__nombre" placeholder="Escriba su correo electrónico" required data-input="email" />
-                    <span class="formulario-contacto__contenido__span"></span>
-                    <input class="formulario-contacto__contenido__nombre" type="password" placeholder="Escriba su contraseña" data-input="password" required />
-                    <span class="formulario-contacto__contenido__span"></span>
-                    <button class="formulario-contacto__contenido__boton" type="submit" formaction="./menu-administrador.html">Entrar</button>
+                    <legend className="formulario-contacto__contenido__titulo">Ingresar a mi cuenta</legend>
+                    <label for='email' className="text-5xl">E-mail</label>
+                    <input type="email" className="formulario-contacto__contenido__nombre" placeholder="Escriba su correo electrónico" required data-input="email" id="email"/>
+                    <span className="formulario-contacto__contenido__span"></span>
+                    <label for='password'>Contraseña</label>
+                    <input className="formulario-contacto__contenido__nombre" type="password" placeholder="Escriba su contraseña" data-input="password" required id="password"/>
+                    <span className="formulario-contacto__contenido__span"></span>
+                    <button className="formulario-contacto__contenido__boton" type="submit" formaction="./menu-administrador.html">Entrar</button>
 
                 </fieldset>
 
             </form>
 
         </>
- 
+        /*Hasta aquí */
     );
 }

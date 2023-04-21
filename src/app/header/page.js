ba116ececfb9d109e674170f6be4434ec596e76f
aguilax <yo1346>
2023-04-21 09:59:45 -0600

    hover en barra navegación

--- src/app/header/page.js
+++ src/app/header/page.js
@@ -5,14 +5,14 @@ export default function Header() {
         <div className="flex px-8 py-4 items-center justify-between">
             <div className="flex items-center">
                 <img alt="Logo e-commerce" className="w-12" src="/e-commerce.png"></img>
-                <h2 className="text-white uppercase ml-1">E-commerce</h2>
+                <h2 className="text-white uppercase ml-1 font-medium">E-commerce</h2>
             </div>
             <nav>
                 <ul className="flex">
-                    <li className="bg-indigo-500 rounded-md w-36 px-0 py-1 mx-2 text-center"><Link href="#">Inicio</Link></li>
-                    <li className="bg-indigo-500 rounded-md w-36 px-0 py-1 mx-2 text-center"><Link href="#">Productos</Link></li>
-                    <li className="bg-indigo-500 rounded-md w-36 px-0 py-1 mx-2 text-center"><Link href="#">Sobre nosotros</Link></li>
-                    <li className="bg-indigo-500 rounded-md w-36 px-0 py-1 mx-2 text-center"><Link href="#">Contacto</Link></li>
+                    <li className="bg-indigo-500 rounded-md px-2 py-1 mx-2 text-center uppercase font-medium"><Link href="#">Inicio</Link></li>
+                    <li className="hover:bg-indigo-500 hover:rounded-md px-2 py-1 mx-2 text-center uppercase font-medium"><Link href="#">Productos</Link></li>
+                    <li className="hover:bg-indigo-500 hover:rounded-md px-2 py-1 mx-2 text-center uppercase font-medium"><Link href="#">Sobre nosotros</Link></li>
+                    <li className="hover:bg-indigo-500 hover:rounded-md px-2 py-1 mx-2 text-center uppercase font-medium"><Link href="#">Contacto</Link></li>
                 </ul>
             </nav>
             

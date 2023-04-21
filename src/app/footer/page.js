d0a92d93ed413bbbefbb6ccb30466b5a9b6680a1
Hernán Felipe Pérez Díaz <hernanfelipeperezdiaz@gmail.com>
2023-04-21 17:41:10 -0500

    footer avance

--- src/app/footer/page.js
+++ src/app/footer/page.js
@@ -1,4 +1,37 @@
 export default function Footer() {
     
-    return <h1 className="text-5xl">Prueba</h1>
+    return(
+
+        <footer className="flex mt-5">
+
+            <Columna title={'Métodos de pago'}/>
+            <Columna title={'Política y privacidad'}/>
+            <Columna title={'Contacto'}/>
+            <a href="#" className="pr-10">
+
+                <img alt="Logo e-commerce" src="/e-commerce.png" />
+
+            </a>
+
+        </footer>
+    );
+}
+
+function Columna({ title }) {
+    
+    return(
+    
+        <div className="w-full text-center">
+
+            <h2 className="color-footer font-black text-xl">{title}</h2>
+            <ol className="leading-8 mt-4 text-sm">
+
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+
+            </ol>
+
+        </div>
+    );
 }

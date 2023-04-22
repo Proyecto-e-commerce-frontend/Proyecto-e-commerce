815dc31eb119cf21225c7d91c6737da548fb530e
juaniz92 <juaniz.92@hotmail.com>
2023-04-22 01:52:51 -0300

    Se agrego estilos al login

--- src/app/Login/page.js
+++ src/app/Login/page.js
@@ -1,23 +1,24 @@
+import React from "react";
+
 export default function Login() {
-    
+
     return(
 
         /*parte del login */
         <>
-
-            <form action="#" className="">
-
+            <form action="#" className="flex items-center justify-center h-screen">
                 <fieldset className="formulario-contacto__contenido">
-
-                    <legend className="formulario-contacto__contenido__titulo">Ingresar a mi cuenta</legend>
-                    <label for='email' className="text-5xl">Email</label>
-                    <input type="email" className="formulario-contacto__contenido__nombre" placeholder="Escriba su correo electrónico" required data-input="email" id="email"/>
-                    <span className="formulario-contacto__contenido__span"></span>
-                    <label for='password'>Contraseña</label>
-                    <input className="formulario-contacto__contenido__nombre" type="password" placeholder="Escriba su contraseña" data-input="password" required id="password"/>
-                    <span className="formulario-contacto__contenido__span"></span>
-                    <button className="formulario-contacto__contenido__boton" type="submit" formaction="./menu-administrador.html">Entrar</button>
-
+                    <div className="flex flex-col">
+                        <legend className="formulario-contacto__contenido__titulo text-2xl mb-2">Ingresar a mi cuenta</legend>
+                        <label for='email' className="text-1">E-mail</label>
+                        <input type="email" className="formulario-contacto__contenido__nombre rounded-md mb-2 p-1 text-black bg-gray-200" placeholder="Escriba su correo electrónico" data-input="email" id="email"/>
+                        <span className="formulario-contacto__contenido__span"></span>
+                        <label for='password' className="text-1">Contraseña</label>
+                        <input className="formulario-contacto__contenido__nombre rounded-md mb-1 p-1 text-black bg-gray-200" type="password" placeholder="Escriba su contraseña" data-input="password" id="password"/>
+                        <span className="formulario-contacto__contenido__span"></span>
+                        <button className="bg-indigo-500 rounded-md formulario-contacto__contenido__boton text-2 m-2 p-1" type="submit" formaction="./menu-administrador.html">INGRESAR</button>
+                        <a href="#" className="flex self-center m-2 hover:text-indigo-500"><strong>REGISTRARSE</strong></a>
+                    </div>
                 </fieldset>
 
             </form>

5c5a5f62c1a5871d71fb4ea3a5baffe45fc73020
Hernán Felipe Pérez Díaz <hernanfelipeperezdiaz@gmail.com>
2023-04-21 20:21:18 -0500

    footer final

--- src/app/footer/page.js
+++ src/app/footer/page.js
@@ -1,37 +1,51 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShop } from "@fortawesome/free-solid-svg-icons";
+import { faBuildingShield } from "@fortawesome/free-solid-svg-icons";
+import { faIdCard } from "@fortawesome/free-solid-svg-icons";
+
 export default function Footer() {
     
     return(
 
-        <footer className="flex mt-5">
+            <footer className="flex mt-5">
 
-            <Columna title={'Métodos de pago'}/>
-            <Columna title={'Política y privacidad'}/>
-            <Columna title={'Contacto'}/>
-            <a href="#" className="pr-10">
+                <Columna title={'Métodos de pago'} icon={faShop}/>
+                <Columna title={'Política y privacidad'} icon={faBuildingShield}/>
+                <Columna title={'Contacto'} icon={faIdCard}/>
+                <a href="#" className="pr-10">
 
-                <img alt="Logo e-commerce" src="/e-commerce.png" />
+                    <img alt="Logo e-commerce" src="/e-commerce.png" />
 
-            </a>
+                </a>
 
-        </footer>
+            </footer>
     );
 }
 
-function Columna({ title }) {
+function Columna({ title, icon }) {
     
     return(
-    
-        <div className="w-full text-center">
 
-            <h2 className="color-footer font-black text-xl">{title}</h2>
-            <ol className="leading-8 mt-4 text-sm">
+            <div className="w-full text-center [&:not(:nth-child(3))]:border-r-4 border-solid border-white">
+                <div className="flex justify-center">
 
-                <li>Lorem ipsum dolor sit amet.</li>
-                <li>Lorem ipsum dolor sit amet.</li>
-                <li>Lorem ipsum dolor sit amet.</li>
+                    <FontAwesomeIcon icon={icon} style={{
 
-            </ol>
+                        width: '8%',
+                        marginRight: '1.2rem'
+                    }} />
+                    <h2 className="color-footer font-black text-xl">{title}</h2>
 
-        </div>
+                </div>
+                <ol className="leading-8 mt-4 text-sm ">
+
+                    <hr className="w-3/4 mr-auto ml-auto"/>
+                    <li>Lorem ipsum dolor sit amet.</li>
+                    <li>Lorem ipsum dolor sit amet.</li>
+                    <li>Lorem ipsum dolor sit amet.</li>
+
+                </ol>
+
+            </div>
     );
 }

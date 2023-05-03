deac54e557832052bee6f9e0c00c3e91a08a8c86
juaniz92 <juaniz.92@hotmail.com>
2023-05-03 17:55:29 -0300

    Se agregó ReCaptcha a páginas de login y registro

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,37 @@
-import React from "react";
+"use client";
+import React, {useRef, useState} from 'react';
 import Inputs from "./components/login y registro/inputs.js";
+import ReCAPTCHA from "react-google-recaptcha";
 
-export default function Login() {
+export default Login;
+
+function Login() {
+
+    const [captchaValido, cambiarCaptchaValido] = useState(null);
+    const [usuarioValido, cambiarUsuarioValido] = useState(false);
+
+    const captcha = useRef(null);
+
+    const onChange = () => {
+		if(captcha.current.getValue()){
+			console.log('El usuario no es un robot');
+            cambiarCaptchaValido(true);
+		}
+	}
+
+    const submit = (e) => {
+		e.preventDefault();
+
+		if(captcha.current.getValue()){
+			console.log('El usuario no es un robot');
+            cambiarUsuarioValido(true);
+            cambiarCaptchaValido(true);
+		} else {
+			console.log('Por favor acepta el captcha');
+            cambiarUsuarioValido(false);
+            cambiarCaptchaValido(false);
+		}
+	}
 
     return(
 
@@ -9,15 +39,22 @@ export default function Login() {
 
             <div>
                 <video src="/fondo-login.mp4" className="mt-20 opacity-30" autoPlay='true' muted loop></video>
-                <form action="#" className="flex items-center justify-center mt-[-60rem] absolute w-full">
+                <form action="#" className="flex items-center justify-center mt-[-60rem] absolute w-full" onSubmit={submit}>
                 <fieldset className="formulario-contacto__contenido">
                     <div className="flex flex-col">
-                        <legend className="formulario-contacto__contenido__titulo text-2xl mb-2">Ingresar a mi cuenta</legend>
+                        <legend className="formulario-contacto__contenido__titulo text-2xl mb-2 self-center">Ingresar a mi cuenta</legend>
                         <label for='email' className="text-1">E-mail</label>
-                        <Inputs tipo='email' clases='formulario-contacto__contenido__nombre rounded-md mb-2 p-1 text-black bg-gray-200 w-[17rem] mb-2' placeholder='Escriba su correo electrónico' data='email' id='email' />
+                        <Inputs tipo='email' clases='formulario-contacto__contenido__nombre rounded-md p-1 text-black bg-gray-200 mb-2' placeholder='Escriba su correo electrónico' data='email' id='email' />
                         <label for='password' className="text-1">Contraseña</label>
-                        <Inputs tipo='password' clases='formulario-contacto__contenido__nombre rounded-md mb-1 p-1 text-black bg-gray-200 mb-2' placeholder='Escriba su contraseña' data='password' id='password' />
+                        <Inputs tipo='password' clases='formulario-contacto__contenido__nombre rounded-md p-1 text-black bg-gray-200 mb-2' placeholder='Escriba su contraseña' data='password' id='password' />
                         <button className="bg-indigo-500 rounded-md formulario-contacto__contenido__boton text-2 m-2 p-1" type="submit" formaction="./menu-administrador.html">INGRESAR</button>
+                        <div className="recaptcha">
+							<ReCAPTCHA
+								ref={captcha}
+								sitekey="6LfcT9wlAAAAAKGmYq-KUn0iEor-e92KiMpIdNtz"
+								onChange={onChange}/>
+						</div>
+                        {captchaValido === false && <div className="text-red-500 mb-5">Por favor acepta el captcha</div>}
                         <a href="./registro" className="flex self-center m-2 hover:text-indigo-500"><strong>REGISTRARSE</strong></a>
                     </div>
                 </fieldset>
--- src/app/registro/page.js
+++ src/app/registro/page.js
@@ -1,17 +1,50 @@
-export default function Registro(){
+"use client";
+import React, {useRef, useState} from 'react';
+import ReCAPTCHA from "react-google-recaptcha";
+
+export default Registro;
+
+function Registro(){
+
+    const [captchaValido, cambiarCaptchaValido] = useState(null);
+    const [usuarioValido, cambiarUsuarioValido] = useState(false);
+
+    const captcha = useRef(null);
+
+    const onChange = () => {
+		if(captcha.current.getValue()){
+			console.log('El usuario no es un robot');
+            cambiarCaptchaValido(true);
+		}
+	}
+
+    const submit = (e) => {
+		e.preventDefault();
+
+		if(captcha.current.getValue()){
+			console.log('El usuario no es un robot');
+            cambiarUsuarioValido(true);
+            cambiarCaptchaValido(true);
+		} else {
+			console.log('Por favor acepta el captcha');
+            cambiarUsuarioValido(false);
+            cambiarCaptchaValido(false);
+		}
+	}
+
     return (
 
         <div>
 
             <video src="/fondo-login.mp4" className="mt-20 opacity-30" autoPlay='true' muted loop></video>
-            <form action="#" className="flex items-center justify-center mt-[-60rem] absolute w-full">
+            <form action="#" className="flex items-center justify-center mt-[-60rem] absolute w-full" onSubmit={submit}>
                     <fieldset className="formulario-contacto__contenido">
                         <div className="flex flex-col">
                         <legend className="formulario-contacto__contenido__titulo text-2xl mb-2">Formulario de registro</legend>
                             <div className="flex justify-between gap-36">
                                 <div className="flex flex-1 flex-col">
                                     <label for='nombre' className="text-1">Nombre</label>
-                                    <input type="text" className="formulario-contacto__contenido__nombre rounded-md mb-2 p-1 text-black bg-gray-200" placeholder="Escriba su nombre" data-input="text" id="nombre"/>
+                                    <input type="text" className="formulario-contacto__contenido__nombre rounded-md p-1 text-black bg-gray-200 mb-2" placeholder="Escriba su nombre" data-input="text" id="nombre"/>
                                     <span className="formulario-contacto__contenido__span"></span>
 
                                     <label for='email' className="text-1">E-mail</label>
@@ -48,18 +81,22 @@ export default function Registro(){
                                     <input type="number" className="formulario-contacto__contenido__nombre rounded-md mb-2 p-1 text-black bg-gray-200" placeholder="Escriba su código postal" data-input="number" id="postal"/>
                                     <span className="formulario-contacto__contenido__span"></span>
 
-
                                     <label for='password' className="text-1">Repetir contraseña</label>
                                     <input className="formulario-contacto__contenido__nombre rounded-md mb-1 p-1 text-black bg-gray-200" type="password" placeholder="Repita su contraseña" data-input="password" id="reppassword"/>
                                     <span className="formulario-contacto__contenido__span"></span>
                                 </div>
                             </div>
-                            <a href="#" className="flex self-center m-2 hover:text-indigo-500"><strong>REGISTRARSE</strong></a>
+                            <button className="bg-indigo-500 rounded-md formulario-contacto__contenido__boton text-2 m-2 p-1 self-center" type="submit" formaction="./menu-administrador.html">INGRESAR</button>
+                            <div className="recaptcha self-center m-5">
+							<ReCAPTCHA
+								ref={captcha}
+								sitekey="6LfcT9wlAAAAAKGmYq-KUn0iEor-e92KiMpIdNtz"
+								onChange={onChange}/>
+						    </div>
+                            {captchaValido === false && <div className="flex self-center text-red-500 mb-5">Por favor acepta el captcha</div>}
                         </div>
                     </fieldset>
-
                 </form>
-
         </div>
     );
 }

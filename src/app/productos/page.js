8972ea03d2137adbc365067601245cd4ffc4f125
juaniz92 <juaniz.92@hotmail.com>
2023-05-05 23:23:46 -0300

    Creación componente productos

---
+++ src/app/productos/page.js
@@ -0,0 +1,59 @@
+import Image from "next/image";
+
+export default function Productos(){
+    return (
+        <div className="flex flex-col h-full p-6">
+            <h2 className="text-4xl p-6">Producto</h2>
+            
+            <div className="flex justify-between gap-10">
+                <div className="flex flex-row justify-between w-1/2 border-2">
+                    <div className="relative h-full w-1/3">
+                        <Image alt="tienda1" fill={true} src="/img-snosotros/tienda1.png"></Image>
+                    </div>
+                        
+                    <div className="p-4">
+                        <h3><strong>Nombre producto</strong></h3>
+                        <h4>Detalles:</h4>
+                        <ul>
+                            <li>Lorem LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem</li>
+                            <li>Lorem LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem</li>
+                            <li>Lorem LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem</li>
+                            <li>Lorem LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem</li>
+                        </ul>
+                        
+                        <div className="flex flex-row justify-between">
+                            <span className="ml-10 m-5 text-3xl">$$$$</span>
+                            <button className="bg-indigo-500 rounded-md formulario-contacto__contenido__boton text-2 m-5 ml-10 px-8" type="submit" formaction="./menu-administrador.html"><strong>COMPRAR</strong></button> 
+                        </div>
+                        
+                    </div>
+                </div>
+
+                <div className="flex flex-row justify-between w-1/2 border-2">
+                    <div className="relative h-full w-1/3">
+                        <Image alt="tienda1" fill={true} src="/img-snosotros/tienda2.png"></Image>
+                    </div>
+                        
+                    <div className="p-4">
+                        <h3><strong>Nombre producto</strong></h3>
+                        <h4>Detalles:</h4>
+                        <ul>
+                            <li>Lorem LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem</li>
+                            <li>Lorem LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem</li>
+                            <li>Lorem LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem</li>
+                            <li>Lorem LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem</li>
+                        </ul>
+                        
+                        <div className="flex flex-row justify-between">
+                            <span className="ml-10 m-5 text-3xl">$$$$</span>
+                            <button className="bg-indigo-500 rounded-md formulario-contacto__contenido__boton text-2 m-5 ml-10 px-8" type="submit" formaction="./menu-administrador.html"><strong>COMPRAR</strong></button> 
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    
+        );
+    }
